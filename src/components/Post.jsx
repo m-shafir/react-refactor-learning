98d4080856d007df1ca848a7562ccf4811d0d4d7
muhammed shafir <muhammedshafirweb@gmail.com>
2025-10-28 20:46:55 +0530

    module style implemented for post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,10 @@
+import styles from "./Post.module.css";
+
 function Post({ author, body }) {
   return (
-    <div className="my-12">
-      <p>{author}</p>
-      <p>{body}</p>
+    <div className={styles.post}>
+      <p className={styles.author}>{author}</p>
+      <p className={styles.text}>{body}</p>
     </div>
   );
 }
