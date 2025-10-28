8531cf3ccff9855d679cf701f85ce2965a41b9db
muhammed shafir <muhammedshafirweb@gmail.com>
2025-10-28 21:28:44 +0530

    added change function and state for body text from new post form

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,12 +1,18 @@
+import { useState } from "react";
 import styles from "./NewPost.module.css";
 
 function NewPost() {
+  const [enteredBody, setEnteredBody] = useState("");
+  function changeBodyHandler(e) {
+    setEnteredBody(e.target.value);
+  }
   return (
     <form className={styles.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} />
+        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
       </p>
+      <p>{enteredBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
