063eb06126aed3ca6587df6594bec22cbcc21046
muhammed shafir <muhammedshafirweb@gmail.com>
2025-10-28 21:00:29 +0530

    NewPost component created and its Style module also

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,12 +1,16 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 import styles from "./PostList.module.css";
 
 function PostList() {
   return (
-    <ul className={styles.posts}>
-      <Post author="Merin Cyriac" body="React is Awesome" />
-      <Post author="Muhammed Shafir" body="React with NEXT JS is Superb!!!" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={styles.posts}>
+        <Post author="Merin Cyriac" body="React is Awesome" />
+        <Post author="Muhammed Shafir" body="React with NEXT JS is Superb!!!" />
+      </ul>
+    </>
   );
 }
 
